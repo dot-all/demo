90c412be63aed4256f8c0e957c8bc44ab0cc6fb0
dot-all <al.quirozsilva@gmail.com>
2024-07-25 22:10:00 -0400

    feat: add hero section

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="h-svh w-10/12 mx-auto flex items-center ">
+      <h1 className="text-8xl">Allan Quiroz</h1>
+    </section>
+  )
+}
