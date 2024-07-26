833b0b56262e901e231ca84b6fa7897d8ff50cc0
dot-all <al.quirozsilva@gmail.com>
2024-07-25 22:10:31 -0400

    feat: add portfolio card component

---
+++ src/components/portfolio/PortfolioCard.tsx
@@ -0,0 +1,72 @@
+import { useRef } from "react";
+import { motion, useInView, Variants } from "framer-motion";
+
+interface Project {
+  name: string;
+  img: string;
+  stack: string[];
+}
+
+interface ProjectCardProps {
+  project: Project;
+  isHovered: boolean;
+  onHoverStart: () => void;
+  onHoverEnd: () => void;
+};
+
+const imageVariants: Variants = {
+  init: { scale: 1 },
+  hovered: { scale: 1.1 }
+};
+
+const elVariants: Variants = {
+  init: { y: 100, opacity: 0 },
+  show: { y: 0, opacity: 1 }
+};
+
+
+const PortfolioCard: React.FC<ProjectCardProps> = ({ project, isHovered, onHoverStart, onHoverEnd }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref, { amount: 0.1, once: true });
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={elVariants}
+      initial="init"
+      animate={isInView ? "show" : "init"}
+      transition={{ duration: 0.4, type: "tween", ease: "easeInOut" }}
+      className="flex bg-[#DADFF7] w-full h-[480px] md:h-[620px] rounded-3xl hover:shadow-xl transition-shadow duration-700 overflow-hidden"
+      onHoverStart={onHoverStart}
+      onHoverEnd={onHoverEnd}
+    >
+      <motion.section 
+        className="flex flex-col w-full"
+        variants={elVariants}
+        initial="init"
+        animate={isInView ? "show" : "init"}
+        transition={{ duration: 1.5, type: "tween", ease: "easeInOut" }}
+      >
+        <motion.img 
+          alt={`Image of project ${project.name}`}
+          animate={isHovered ? "hovered" : "init"}
+          className="w-80 h-80 m-auto"
+          initial="init"
+          transition={{ duration: 0.45, type: "tween", ease: "easeInOut" }}
+          src={project.img}
+          variants={imageVariants}
+          />
+        <section className="mt-auto flex flex-col ml-10 mb-10">
+          <h3 className="font-semibold text-3xl pb-3">{project.name}</h3>
+          <ul className="flex flex-row gap-2">
+            {project.stack.map((tech, index) => (
+              <li className="rounded-full border-black border text-sm px-3 py-1 font-medium" key={index}>{tech}</li>
+            ))}
+          </ul>
+        </section>
+      </motion.section>
+    </motion.div>
+  );
+};
+
+export default PortfolioCard;
