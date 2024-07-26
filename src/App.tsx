8e4bb5f786f24bf4b06d102a643d1f4ad007c13d
dot-all <al.quirozsilva@gmail.com>
2024-07-25 22:08:17 -0400

    build: init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+import Sidebar from "./components/sidebar";
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1 className='text-2xl w-fit mx-auto'>Hola mundo!</h1>
+        <Sidebar />
+      </main>
+    </>
+  )
+}
+
+export default App
