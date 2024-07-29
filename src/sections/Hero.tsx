5ddb28df4c9425f9cd6acfefee99d7dbf5564fd2
dot-all <al.quirozsilva@gmail.com>
2024-07-28 23:21:50 -0400

    feat: add new background to hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,36 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
+import "./hero.scss"
+
 export default function Hero() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  });
+  
+  const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
+
   return (
-    <section className="h-svh w-10/12 mx-auto flex items-center ">
-      <h1 className="text-8xl">Allan Quiroz</h1>
+    <section ref={ref} className="h-screen w-full mx-auto grid grid-rows-3 relative">
+      <div className="gradient-bg">
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+        </div>
+      </div>
+      <div className="font-medium row-start-2 z-10 w-10/12 mx-auto">
+        <h1 className="text-6xl md:text-8xl">Allan Quiroz.</h1>
+        <h2 className="text-5xl md:text-7xl">Desarrollador Fullstack</h2>
+      </div>
+      <motion.p
+        className="row-start-3 mt-auto mx-auto mb-5 font-semibold"
+        style={{ opacity }}
+      >
+        HAZ SCROLL PARA DESCUBRIR
+      </motion.p>
     </section>
-  )
+  );
 }
