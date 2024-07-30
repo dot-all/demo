68e3a17b9eadd476fd33ba609de3cb211c4793ad
dot-all <al.quirozsilva@gmail.com>
2024-07-29 22:19:07 -0400

    chore: update background color

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,18 +1,20 @@
-// import { Link } from "react-router-dom";
-
-import DropDownMenu from "../components/ui/DropdownMenu";
+import Header from "../components/ui/Header";
 import ProgressBar from "../components/ui/ProgressBar";
+import About from "../sections/About";
 import Contact from "../sections/Contact";
+import Experience from "../sections/Experience";
 import Hero from "../sections/Hero";
 import Portfolio from "../sections/Portfolio";
 
 export default function Root() {
   return (
     <>
-      <main className="w-full h-full pb-10 bg-[#f0f1fa]">
-        <DropDownMenu />
+      <main className="w-full h-full pb-10">
+        <Header />
         <Hero />
+        <Experience />
         <Portfolio />
+        <About />
         <Contact />
         <ProgressBar />
       </main>
