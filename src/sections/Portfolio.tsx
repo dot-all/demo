72d461ab66e5d6db7166cd4189d41a85d51a9244
dot-all <al.quirozsilva@gmail.com>
2024-07-29 22:21:19 -0400

    chore: update Project interface

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -2,11 +2,13 @@ import { motion, useInView, Variants } from "framer-motion";
 import { useRef, useState } from "react";
 
 import PortfolioCard from "../components/portfolio/PortfolioCard";
+import AnimatedHeading from "../components/ui/AnimatedHeading";
 
 interface Project {
   name: string;
   img: string;
   stack: string[];
+  url: string;
 }
 
 const textVariants: Variants = {
@@ -19,23 +21,27 @@ const textVariants: Variants = {
 const projects: Project[] = [
   {
     name: 'Shortie',
-    img: 'img/project-1.webp',
+    img: 'img/shortie-web.webp',
     stack: ['React', 'NodeJS', 'Shadcn'],
+    url: "https://shortie-urls.vercel.app/"
   },
   {
     name: 'Project B',
     img: 'img/project-2.png',
     stack: ['React', 'NodeJS', 'Shadcn'],
+    url: "http://"
   },
   {
     name: 'Project C',
     img: 'img/project-3.png',
     stack: ['React', 'NodeJS', 'Shadcn'],
+    url: "http://"
   },
   {
     name: 'Project D',
     img: 'img/project-4.png',
     stack: ['React', 'NodeJS', 'Shadcn'],
+    url: "http://"
   },
 ];
 
@@ -43,22 +49,12 @@ const projects: Project[] = [
 
 const Portfolio: React.FC = () => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  const ref = useRef<HTMLDivElement | null>(null);
-  const isInView = useInView(ref, { amount: 0.1, once: true });
-
   return (
-    <section className="min-h-svh w-11/12 xl:w-10/12 mx-auto grid grid-cols-1 xl:grid-cols-2 gap-8 justify-center my-28 max-w-[1300px]">
-      <motion.h2
-        ref={ref}
-        initial= "init"
-        variants={textVariants}
-        animate={ isInView ? "show": "init"}
-        className="col-span-1 xl:col-span-2 flex"
-        transition={{ duration: 0.45, type: "tween", ease: "easeInOut" }}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-6" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32" d="M400 352l64-64-64-64"/><path d="M448 288H154c-58.76 0-106-49.33-106-108v-20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg>
-        <span className="ml-4 text-2xl">P R O Y E C T O S</span>
-      </motion.h2>
+    <section className="min-h-svh w-11/12 xl:w-10/12 mx-auto grid grid-cols-1 xl:grid-cols-2 gap-8 justify-center my-28 max-w-[1300px]" id="portfolio">
+      <AnimatedHeading 
+        text="P R O Y E C T O S" 
+        className="col-span-1 xl:col-span-2"
+      />
       {projects.map((project, index) => (
         <PortfolioCard 
           key={index}
