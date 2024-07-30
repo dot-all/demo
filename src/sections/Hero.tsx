3a4f030b405cc784a4ea7c27aba4e4be4012919e
dot-all <al.quirozsilva@gmail.com>
2024-07-29 22:20:52 -0400

    feat: update Hero with introductory sentence about me

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
   const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
 
   return (
-    <section ref={ref} className="h-screen w-full mx-auto grid grid-rows-3 relative">
+    <section ref={ref} className="h-svh w-full mx-auto grid grid-rows-3 relative">
       <div className="gradient-bg">
         <div className="gradients-container">
           <div className="g1"></div>
@@ -22,11 +22,11 @@ export default function Hero() {
         </div>
       </div>
       <div className="font-medium row-start-2 z-10 w-10/12 mx-auto">
-        <h1 className="text-6xl md:text-8xl">Allan Quiroz.</h1>
-        <h2 className="text-5xl md:text-7xl">Desarrollador Fullstack</h2>
+        <h1 className="text-6xl md:text-8xl mb-4">Allan Quiroz.</h1>
+        <p className="text-xl md:text-3xl md:w-10/12">1 año de experiencia. <span className="text-[#0814ec]">Ingeniero en informática</span> de Viña del Mar, Chile. Especializado en hacer experiencias web únicas.</p>
       </div>
       <motion.p
-        className="row-start-3 mt-auto mx-auto mb-5 font-semibold"
+        className="text-sm md:text-base row-start-3 mt-auto mx-auto mb-5 font-semibold"
         style={{ opacity }}
       >
         HAZ SCROLL PARA DESCUBRIR
